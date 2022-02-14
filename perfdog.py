6455f732893a0f92ad791e16e861bf34f2c69d38
hankfeng <hankfeng@tencent.com>
2022-02-14 19:33:46 +0800

    优化使用终端启动测试之后关闭终端service不再关闭，取消开始测试获取app列表的优化

--- perfdog.py
+++ perfdog.py
@@ -63,7 +63,7 @@ class Service(object):
 
     @staticmethod
     def __startup(service_path):
-        subprocess.Popen(service_path)
+        subprocess.Popen(service_path, shell=True, creationflags=subprocess.CREATE_NEW_CONSOLE)
 
     @staticmethod
     def __create_stub_factory():
@@ -340,7 +340,10 @@ class TestAppBuilder(TestTargetBuilder):
         self.__sub_window = sub_window
 
     def build(self):
-        app = perfdog_pb2.AppInfo(packageName=self.__package_name)
+        app = self.device().get_app(self.__package_name)
+        if app is None:
+            raise NonExistTestTarget()
+
         req = perfdog_pb2.StartTestAppReq(device=self.device().real_device(),
                                           app=app,
                                           subProcess=self.__sub_process_name,
